9f063098f1911af8f81f2f341fe74d1c36e8a2a2
sim1gariglio <simone.gariglio1@gmail.com>
2024-06-20 12:34:49 +0200

    we

--- ex03/DiamondTrap.cpp
+++ ex03/DiamondTrap.cpp
@@ -6,17 +6,10 @@
 /*   By: sgarigli <sgarigli@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/20 11:28:13 by sgarigli          #+#    #+#             */
-/*   Updated: 2024/06/20 11:41:26 by sgarigli         ###   ########.fr       */
+/*   Updated: 2024/06/20 12:30:57 by sgarigli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "DiamondTrap.hpp"
 
-DiamondTrap::DiamondTrap(std::string name) : Claptrap(name + "_clap_name"), Scavtrap(name), Fragtrap(name)
-{
-	this->_name = name;
-	this->_hitPoints = F_HP;
-	this->_energyPoints = S_EP;
-	this->_attackDamage = F_AD;
-	std::cout << CYAN << "DiamondTrap constructor called" << std::endl;
-}
+
--- ex03/main.cpp
+++ ex03/main.cpp
@@ -6,19 +6,21 @@
 /*   By: sgarigli <sgarigli@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/20 09:15:33 by sgarigli          #+#    #+#             */
-/*   Updated: 2024/06/20 11:12:36 by sgarigli         ###   ########.fr       */
+/*   Updated: 2024/06/20 12:20:02 by sgarigli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ClapTrap.hpp"
 #include "ScavTrap.hpp"
 #include "FragTrap.hpp"
+#include "DiamondTrap.hpp"
 
 int main()
 {
 	Claptrap claptrap("sgarigli");
 	Scavtrap scavtrap("gduranti");
 	Fragtrap fragtrap("mruggier");
+	DiamondTrap diamondtrap("lmicheli");
 
 	claptrap.attack(scavtrap);
 	scavtrap.attack(claptrap);
