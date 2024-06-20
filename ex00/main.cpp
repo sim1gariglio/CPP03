8b7c6687936e7e9e05f08a9d5f83a35ce335c796
sim1gariglio <simone.gariglio1@gmail.com>
2024-06-20 10:16:00 +0200

    better

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: sgarigli <sgarigli@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/20 09:15:33 by sgarigli          #+#    #+#             */
-/*   Updated: 2024/06/20 10:11:05 by sgarigli         ###   ########.fr       */
+/*   Updated: 2024/06/20 10:15:38 by sgarigli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,6 @@ int main()
 	Claptrap claptrap("Simone");
 	Claptrap claptrap2("sgarigli");
 	
-	std::cout << claptrap.getName() << " has " << claptrap.getHitPoints() << " hit points, " << claptrap.getEnergyPoints() << " energy points and " << claptrap.getAttackDamage() << " attack damage." << std::endl;
-	std::cout << claptrap2.getName() << " has " << claptrap2.getHitPoints() << " hit points, " << claptrap2.getEnergyPoints() << " energy points and " << claptrap2.getAttackDamage() << " attack damage." << std::endl;
 	claptrap.attack(claptrap2);
 	claptrap2.takeDamage(3);
 	claptrap2.beRepaired(5);
